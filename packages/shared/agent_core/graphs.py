821ab73fb8bd08eff69ab11652f54506e8d5b650
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-05-19 18:18:57 +0000

    building graph

---
+++ packages/shared/agent_core/graphs.py
@@ -0,0 +1,25 @@
+def decide_to_finish(state: RefactorState):
+    """Router to decide if we loop back or end."""
+    if state["errors"] == "None" or state["iterations"] > 3:
+        return "end"
+    return "rewrite"
+
+workflow = StateGraph(RefactorState)
+
+workflow.add_node("reviewer", code_review)
+workflow.add_node("refactorer", code_refactorer)
+workflow.add_node("linter", code_linter)
+
+workflow.set_entry_point("reviewer")
+workflow.add_edge("reviewer", "refactorer")
+workflow.add_edge("refactorer", "linter")
+
+# Conditional Logic
+workflow.add_conditional_edges(
+    "linter",
+    decide_to_finish,
+    {
+        "rewrite": "refactorer", # Loop back
+        "end": END              # Exit
+    }
+)
