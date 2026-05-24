1cf3b243779145c91ea8506558410abb008c9ab8
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-05-24 14:42:33 +0000

    pushing diff.py file for db/models

---
+++ packages/db/models/diff.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
+import uuid
+
+class DiffModel(SQLModel , table=True):
+    __tablename__= "diffs"
+
+    id:str =Field(
+        defaut_factory=lambda:str(uuid.uuid4()),
+        primary_key=True
+    )
+
+    session_id:str=Field(foreign_key="Sessions.id",index=True)
+    status: str=Field(default="pending")
+    original_code:str
+    refactored_code:str
+    unified_diff:str
+    lines_added:int=Field(default_factory=fatetime.utcnow)
+    resolved_at: Optional[datetime]=Field(default=None)
