116abb894d9a73d0bc4c5e70bfda834539f3ab4d
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-09-17 01:33:26 +0530

    Remove tracked build artifacts already covered by .gitignore

--- apps/api/apps/ide-extension/out/src/session/session_manager.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SessionManager = void 0;
-var SessionManager_1 = require("../services/SessionManager");
-Object.defineProperty(exports, "SessionManager", { enumerable: true, get: function () { return SessionManager_1.SessionManager; } });
